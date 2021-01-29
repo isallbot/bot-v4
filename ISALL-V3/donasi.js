408ac13b7cccf2a66aa212397c84d393a5c78ff2
isallbot <78047216+isallbot@users.noreply.github.com>
2021-01-29 16:36:09 +0700

    Add files via upload

---
+++ ISALL-V3/donasi.js
@@ -0,0 +1,22 @@
+const donasi = (prefix) => {
+	return `╔════════════════════
+║ *DONASI UNTUK BOT ISALL*
+╠════════════════════
+║╭──❉ *DONASI BOS* ❉─────
+║│➸ *SAWERIA*: https://saweria.co/BOTISALL
+║│➸ *DANA*: _0896-3026-7618_
+║│➸ *PULSA*: _0896-3026-7618_
+║│➸ *GOPAY*: _0896-3026-7618_
+║╰──────────────────
+╠════════════════════
+║         
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║        |FAISAL WAPI|
+╠════════════════════
+║ _*COPYRIGHT BY FAISAL WAPI*_
+╚════════════════════
+`
+}
+
+exports.donasi = donasi
