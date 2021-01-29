948b803dfa851f553fc076cb57dec51efc10150a
isallbot <78047216+isallbot@users.noreply.github.com>
2021-01-29 16:52:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ async function starts() {
 			const isNsfw = isGroup ? nsfw.includes(from) : false
 			const isSimi = isGroup ? samih.includes(from) : false
 			const isOwner = ownerNumber.includes(sender)
-			const isISALLGANS = isallNumber.includes(sender)
+			const isISALLGANS = ISALLNumber.includes(sender)
 			const isUrl = (url) => {
 			    return url.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/, 'gi'))
 			}
